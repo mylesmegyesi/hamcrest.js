fd67c63a4fd751ce146acefd0d5b38f374504445
Myles Megyesi <mylesmegyesi@users.noreply.github.com>
2017-11-30 14:21:28 -0600

    Adds is equality matcher (Object.is)

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ export { assertThat, AssertionError } from "./MatcherAssert";
 
 export { any } from "./matchers/Any";
 export { equalTo } from "./matchers/EqualTo";
+export { is } from "./matchers/Is";
 export { looselyEqualTo } from "./matchers/LooselyEqualTo";
 export { matcherMatches, matcherDoesNotMatch } from "./matchers/MatchResult";
 export { strictlyEqualTo } from "./matchers/StrictlyEqualTo";
